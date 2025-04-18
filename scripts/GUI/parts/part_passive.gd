0b47b15ecc9eaf25b36dfea25fa5ce4498118a73
Creepslime <creepslime@gmail.com>
2025-04-18 19:59:39 -0400

    Nuke

--- scripts/GUI/parts/part_passive.gd
+++
@@ -1,5 +0,0 @@
-extends Part
-
-class_name PartPassive
-
-##This script just exists to create the base class
--- scripts/objects/npcs/baseEnemy/enemy.gd
+++
@@ -1,9 +0,0 @@
-extends Combatant;
-
-class_name EnemyBase
-
-func _get_AI_handler():
-	return get_node_or_null("AIHandler");
-	
-func take_damage(damage):
-	get_node("CombatHandler").take_damage(damage);
